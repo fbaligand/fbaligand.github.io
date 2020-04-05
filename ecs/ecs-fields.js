11d9cf1305806a0285fadf6c327b016a11ea14ba
fbaligand <fbaligand@gmail.com>
2020-04-05 10:11:55 +0200

    add ECS fields table with search bar
    fix responsive design

---
+++ ecs/ecs-fields.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $("#ecs-fields").DataTable({
+        "ajax": {
+            "url": "./ecs-fields.json",
+            "dataSrc": ""
+        },
+        "paging": false,
+        "responsive": true,
+        "fixedHeader": true,
+        "language": {
+            "info": "Showing _END_ fields",
+            "infoEmpty": "No fields found",
+            "infoFiltered": "(filtered from _MAX_ total fields)"
+        },
+        "order": [[1, "asc"]],
+        "columns": [
+            { "data": "fieldName", "title": "Field Name" },
+            { "data": "fieldSet", "title": "Field Set" },
+            { "data": "fieldLevel", "title": "Field Level" },
+            { "data": "fieldDescription", "title": "Field Description", "orderable": false, "searchable": false }
+        ]
+    });
+});
