1c9af047faf4d840fb5779b2d61c2b2f936ef97f
fbaligand <fbaligand@gmail.com>
2021-09-03 07:37:43 +0200

    add ECS v1.11.0 fields

--- ecs/ecs-fields.js
+++ ecs/ecs-fields.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $("#ecs-fields").DataTable({
         "ajax": {
-            "url": "./ecs-fields.json?v=1.10.0",
+            "url": "./ecs-fields.json?v=1.11.0",
             "dataSrc": ""
         },
         "paging": false,
